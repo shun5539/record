0b02409a8647594f42ef7f70e6245b418bf49136
shun5539 <hatano.shun@piala.co.jp>
2022-12-06 16:11:20 +0900

    add front-ts-vue-setting

---
+++ front-ts/front-ts-vue-setting/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header :title="title"></Header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Header from "./components/Header.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { Header },
+  setup() {
+    const title = ref("Hello World");
+    return { title };
+  },
+});
+</script>
---
+++ front-ts/front-ts-vue-setting/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ name }}</p>
+    <button @click="changeName('TypeScript')">click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {
+    title: {
+      required: true,
+      type: String,
+    },
+  },
+  setup() {
+    const name = ref("JavaScript");
+    return { name };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+  },
+});
+</script>
