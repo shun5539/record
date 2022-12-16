e01832c13ce5500eaf4ee9571b5da3e955bf5dd0
shun5539 <hatano.shun@piala.co.jp>
2022-12-16 13:37:19 +0900

    latest ver

--- front-ts/front-ts-vue-setting/src/App.vue
+++ front-ts/front-ts-vue-setting/src/App.vue
@@ -1,19 +1,12 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Header from "./components/Header.vue";
+
+const title = ref("Hello World");
+</script>
+
 <template>
   <div>
     <Header :title="title"></Header>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from "vue";
-import Header from "./components/Header.vue";
-
-export default defineComponent({
-  name: "App",
-  components: { Header },
-  setup() {
-    const title = ref("Hello World");
-    return { title };
-  },
-});
-</script>
--- front-ts/front-ts-vue-setting/src/components/Header.vue
+++ front-ts/front-ts-vue-setting/src/components/Header.vue
@@ -1,29 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const props = defineProps({
+  title: {
+    required: true,
+    type: String,
+  },
+});
+const name = ref("JavaScript");
+
+const changeName = (word: string) => {
+  name.value = word;
+};
+</script>
+
 <template>
   <div>
-    <h1>{{ title }}</h1>
+    <h1>{{ props.title }}</h1>
     <p>{{ name }}</p>
     <button @click="changeName('TypeScript')">click</button>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, ref } from "vue";
-
-export default defineComponent({
-  props: {
-    title: {
-      required: true,
-      type: String,
-    },
-  },
-  setup() {
-    const name = ref("JavaScript");
-    return { name };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
-  },
-});
-</script>
